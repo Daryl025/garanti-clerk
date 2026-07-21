904fec6f2cd8c97dd82a0f76b13b58a622ecf091
Arold Daryl Tchouala <AroldDaryl.TchoualaMboumanah@edu.sait.ca>
2026-07-20 18:38:30 -0600

    fix: remove broken console.log from search chain

--- screens/clerk/WalkIn.js
+++ screens/clerk/WalkIn.js
@@ -57,7 +57,7 @@ export default function WalkIn({ navigation }) {
     setSeats([]);
     setSelectedSeat(null);
     const ds = formatDate(date);
-    axios.get(`${API}/api/trips/search?origin=${from.code}&destination=${to.code}&date=${ds}&passengers=1`).then(r => { console.log('SEARCH URL:', `${API}/api/trips/search?origin=${from.code}&destination=${to.code}&date=${ds}`); return r; })
+    axios.get(`${API}/api/trips/search?origin=${from.code}&destination=${to.code}&date=${ds}&passengers=1`)
       .then(r => setTrips(r.data?.trips || []))
       .catch(() => setTrips([]));
   }, [from, to, date]);
